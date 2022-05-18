92cb863061b9ee392d367b03f6baf1766e5047a3
Robbe <luisrobbe32@gmail.com>
2022-05-18 09:04:31 +0200

    Actualizado. Lo que da error está comentado, que son los efectos visuales

---
+++ app/src/main/java/com/example/artest_app/MainActivity.java
@@ -0,0 +1,212 @@
+package com.example.artest_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView; //Declaración de la variable de tipo ListView
+    String[] items; //Declaración del array de tipo String con los items
+    //Los items en este caso son los nombres de cada canción
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Identificación de la variable listView ya declarada con el xml del activity main
+        listView = (ListView) findViewById(R.id.listView);
+
+        //Llamando a este método en el onCreate la aplicación pedirá permisos al móvil
+        //para acceder a las canciones
+        runtimePermission();
+    }
+
+    /**
+     * Estos métodos procedentes de la librería Dexter son los que regularán los permisos
+     * de acceso al dispositivo. Podemos ver que si permitimos que la aplicación acceda
+     * al dispositivo llamará al método displaySongs()
+     */
+    public void runtimePermission() {
+        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        displaySong();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    /**
+     * Función de tipo ArrayList que devolverá un arreglo con el listado de canciones
+     * @param file
+     * @return
+     */
+    public ArrayList<File> findSong(File file) {
+        ArrayList<File> arrayList = new ArrayList<>(); //Arraylist de todas la canciones
+
+        File f = file; //Objeto de tipo File
+
+        File[] files = null; //ArrayList de tipo File
+
+        /**
+         * Estructura try/catch. Es muy importante para atrapar el error
+         * de tipo NullpointerException
+         */
+        try {
+
+            files = f.listFiles(); //La variable de tipo File va a listar todos los archivos con listFiles()
+
+            //Si la longitud de los archivos es mayor que 0
+            if (files.length > 0) {
+
+                //Estructura for en la cual se itera cada archivo
+                for (File singleFile : files) {
+
+                    //Si la canción está en el directorio o se encuentra oculta
+                    if (singleFile.isDirectory() && !singleFile.isHidden()) {
+
+                        arrayList.addAll(findSong(singleFile)); //Se añaden todas las canciones encontradas
+                    } else {
+
+                        //Si el nombre de la canción acaba en .mp3 o .wav
+                        if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")) {
+                            arrayList.add(singleFile); //Se añadirá la canción al array
+                        }
+                    }
+                }
+            } else {
+                //Este objeto de tipo Path obtendrá la ruta de los archivos en el móvil
+                Path path = Paths.get("/storage/emulated/0/Music");
+                File filePath = path.toFile();
+                arrayList.add(filePath); //Cada archivo de tipo File se añadirá al arreglo
+            }
+
+        } catch (NullPointerException e) {
+            System.out.println("Error:" + e + "" + e.getStackTrace()[0].getLineNumber());
+        }
+        return arrayList; //Este método nos devolverá un arreglo con todas las canciones
+    }
+
+    /**
+     * Función que mostrará las canciones del directorio del móvil en la aplicación
+     * tras autorizar los permisos de configuración
+     */
+    public void displaySong() {
+        //Constante final mySongs de tipo arrayList con el listado de canciones
+        //Con findSong obtendremos las canciones del almacenamiento externo del móvil
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+
+        //Esta variable se inicializó en las primeras líneas
+        //Se inicializa como un nuevo string mostrando el tamaño de las canciones
+        items = new String[mySongs.size()];
+
+        //Estructura for que iterará cada una de las canciones
+        for (int i = 0; i < mySongs.size(); i++) {
+
+            //La variable items obtendrá el nombre de cada archivo de audio y lo reemplazará por un espacio vacio
+            //De esta manera podrá introducirse cada canción en el array de strings sin mostrar su formato
+            //Esto le dará a la biblioteca de canciones un aspecto más elegante y menos cargado
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+        }
+
+        //Objeto de tipo customadapter
+        customAdapter customAdapter = new customAdapter();
+        listView.setAdapter(customAdapter);
+
+        //El siguiente método hará que al hacer click sobre una canción nos lleve a otra activity
+        /**
+         * La función onItemClick nos permitirá acceder a la Player Activity (al reproductor de
+         * música) cuando estando en la biblioteca de canciones pulsamos sobre una para
+         * escucharla.
+         */
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String songName = (String) listView.getItemAtPosition(position);
+
+                //Aquí se crea un intent para poder acceder a la playeractivity
+                //Haciendo click sobre el array de archivos mySongs
+                startActivity(new Intent(getApplicationContext(), PlayerActivity.class)
+                        .putExtra("songs", mySongs)
+                        .putExtra("songname", songName)
+                        .putExtra("pos", position)
+                );
+            }
+        });
+
+    }
+
+    /**
+     * Este método que hereda de BaseAdapter será aquel que cargará el contenido visual de la aplicación
+     * Esta herencia hace que se sobreescriban estos cuatro métodos que vemos a continuación
+     * Los métodos getItem y getItemId no son necesarios en la aplicación. Es por ello que no los
+     * vamos a implementar.
+     */
+    class customAdapter extends BaseAdapter {
+
+        /**
+         * Obtendrá la longitud de todos los items
+         * @return items.length
+         */
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        /**
+         * Este método es muy importante, ya que nos devolverá la vista de la lista de
+         * canciones inflando el layout.
+         * @param position
+         * @param convertView
+         * @param parent
+         * @return view
+         */
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.list_item, null);
+            TextView txtSong = view.findViewById(R.id.txtSong);
+            txtSong.setSelected(true);
+            txtSong.setText(items[position]);
+            return view;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/artest_app/PlayerActivity.java
@@ -0,0 +1,400 @@
+package com.example.artest_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.gauravk.audiovisualizer.visualizer.BarVisualizer;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    //Declaración de los botones que componen el reproductor de música
+    Button btnPlay, btnNext, btnPrevious, btnFastForward, btnFastBackward;
+
+    //Declaración de los textos relativos a la canción, que marcarán su nombre, comienzo y fin
+    TextView txtSongName, txtSongStart, txtSongEnd;
+
+    //Declaración de la barra de progreso de la canción
+    SeekBar seekMusicBar;
+
+    //Declaración de la variable que definirá los efectos visuales en el reproductor
+    BarVisualizer barVisualizer;
+
+    //Declaración de la imageView (icono de la nota musical del reproductor)
+    ImageView imageView;
+
+    //Declaración de la variable de tipo String songName
+    String songName;
+
+    //Inicialización de la constante del nombre de la canción
+    public static final String EXTRA_NAME = "song_name";
+
+    //Declaración de la variable estática de la clase mediaplayer
+    static MediaPlayer mediaPlayer;
+
+    //Variable que determina la posición que tendrá cada canción dentro del array mySongs
+    int position;
+
+    //Declaración del array de canciones
+    ArrayList<File> mySongs;
+
+    //Declaración de la variable actualizar SeekBar mediante el uso de un hilo o thread
+    Thread updateSeekBar;
+
+    /**
+     * Esta función activa el botón para retroceder a la biblioteca de canciones
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        //Colocación del nombre de la aplicación
+        getSupportActionBar().setTitle("Artest");
+        //Activación del botón para retroceder
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //Colocación del icono de la aplicación
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setIcon(R.drawable.logo);
+
+        //Inicialización de los botones sincronizándolos con su vista en el activity_player.xml
+        btnPrevious = findViewById(R.id.btnPrevious);
+        btnNext = findViewById(R.id.btnNext);
+        btnPlay = findViewById(R.id.btnPlay);
+        btnFastForward = findViewById(R.id.btnFastForward);
+        btnFastBackward = findViewById(R.id.btnFastBackward);
+
+        //Inicialización de los textos visibles en el reproductor
+        txtSongName = findViewById(R.id.txtSong);
+        txtSongStart = findViewById(R.id.txtSongStart);
+        txtSongEnd = findViewById(R.id.txtSongEnd);
+
+        //Inicialización de la barra de progreso sincronizándola con su vista en el xml
+        seekMusicBar = findViewById(R.id.seekBar);
+
+        //Inicialización del efecto visual sincronizándolo con su vista en el xml
+        //barVisualizer = findViewById(R.id.green_wave);
+
+        //Inicialización del imageView sincronizándolo con su vista en el xml
+        //Se trata de la imagen de la nota
+        imageView = findViewById(R.id.imgView);
+
+        //Estructura if en la cual si el mediaPlayer es diferente de null comenzará y podrá pausar
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+            mediaPlayer.release();
+        }
+
+        //Declaración e inicialización del intent que nos dará el intent
+        Intent intent = getIntent();
+
+        //Obtención de los extras a través del bundle
+        Bundle bundle = intent.getExtras();
+
+        //El método adjunto al bundle nos devolverá a la biblioteca de canciones gracias al intent
+        mySongs = (ArrayList)bundle.getParcelableArrayList("songs");
+
+        //Esta variable representa al nombre de la canción que se está reproduciendo
+        String sName = intent.getStringExtra("songname");
+
+        //Esta variable representa a la posición de la canción que se está reproduciendo
+        position = bundle.getInt("pos", 0);
+
+        //txtSongName se hará visible mientras se reproduce el contenido auditivo que lo representa
+        txtSongName.setSelected(true);
+
+        //La uri convertirá el listado de canciones en un string para que podamos escucharlas
+        Uri uri = Uri.parse(mySongs.get(position).toString());
+        songName = mySongs.get(position).getName();
+        txtSongName.setText(songName);
+
+        //mediaplayer creará el contexto gracias al uri y seguidamente la canción comenzará a sonar
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+        mediaPlayer.start();
+
+        /**
+         * Este nuevo hilo sobreescribirá la función run
+         * Dicha función tratará la duración total de la canción y la posición actual del seekBar
+         * Mediante una estructura try se obtendrá la posición actual de la barra de progreso
+         * y esto nos permitirá como se mueve a medida que la canción transcurre
+         */
+        updateSeekBar = new Thread(){
+            @Override
+            public void run() {
+                int totalDuration = mediaPlayer.getDuration();
+                int currentPosition = 0;
+
+                while(currentPosition < totalDuration) {
+
+                    try {
+                        sleep(500);
+                        currentPosition = mediaPlayer.getCurrentPosition();
+                        seekMusicBar.setProgress(currentPosition);
+
+                    } catch (InterruptedException | IllegalStateException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        };
+
+        //Este método nos dará la duración máxima en función de la canción que esté sonando
+        seekMusicBar.setMax(mediaPlayer.getDuration());
+        //Comienzo de la actualización de la barra de progreso
+        updateSeekBar.start();
+        //Estas líneas dibujarán el movimiento de la seekBar con un color celeste
+        seekMusicBar.getProgressDrawable().setColorFilter(getResources().getColor(R.color.teal_200), PorterDuff.Mode.MULTIPLY);
+        seekMusicBar.getThumb().setColorFilter(getResources().getColor(R.color.teal_200), PorterDuff.Mode.SRC_IN);
+
+        /**
+         * setOnSeekBarChangeListener permitirá el libre movimiento de la seekBar para que
+         * podamos seleccionar un determinado fragmento de la canción
+         */
+        seekMusicBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+
+        //Esta variable creará el tiempo y nos dará la duración final de la canción
+        //De esta manera nos devuelve el tiempo en formato textView
+        String endTime = createTime(mediaPlayer.getDuration());
+        txtSongEnd.setText(endTime);
+
+        //Gracias al objeto Handler y a la variable delay se nos mostrará como se modifica el tiempo
+        //Que lleva de progreso la canción cuando movemos la seekBar o pulsamos
+        //Sobre los botones que nos permiten avanzar o ir atrás sobre la canción
+        final Handler handler = new Handler();
+        final int delay = 1000;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String currentTime = createTime(mediaPlayer.getCurrentPosition());
+                txtSongStart.setText(currentTime);
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+
+        /**
+         * Este método onClick hará que cuando clickemos sobre el botón play la canción se pause
+         * o se reanude en función de la siguiente estructura if
+         * La estructura if dará funcionalidad a los botones play y pause según se de el caso
+         */
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Si la canción está sonando y la pausamos, aparecerá el botón play
+                //El botón play es la señal que nos permitirá reanudar la canción por donde iba
+                if(mediaPlayer.isPlaying()){
+                    btnPlay.setBackgroundResource(R.drawable.ic_play);
+                    mediaPlayer.pause();
+                } else {
+                    //Si la canción está en pausa y la reanudamos, aparecerá el botón pause
+                    //El botón pause es la señal que nos permitirá pausar la canción cuando queramos
+                    btnPlay.setBackgroundResource(R.drawable.ic_pause);
+                    mediaPlayer.start();
+
+                    /**
+                     * Después de hacer click sobre la canción y cambiar a la pantalla del
+                     * reproductor de música, el imageView (la imagen de la nota musical)
+                     * generará una animación rotando en círculo
+                     */
+                    TranslateAnimation moveAnim = new TranslateAnimation(-25, 25,-25, 25);
+                    moveAnim.setInterpolator(new AccelerateInterpolator());
+                    moveAnim.setDuration(600);
+                    moveAnim.setFillEnabled(true);
+                    moveAnim.setFillAfter(true);
+                    moveAnim.setRepeatMode(Animation.REVERSE);
+                    moveAnim.setRepeatCount(1);
+
+                    //Aquí estamos transfiriendo la animación al imageView
+                    imageView.startAnimation(moveAnim);
+
+                }
+            }
+        });
+
+        /**
+         *
+         */
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                btnNext.performClick();
+            }
+        });
+
+        //Estas tres líneas se encargan de mostrar el efecto visual en la parte inferior
+        //del reproductor de música cuando esta sonando la canción
+        /*int audioSessionId = mediaPlayer.getAudioSessionId();
+        if (audioSessionId != -1) {
+            barVisualizer.setAudioSessionId(audioSessionId);
+        }*/
+
+        /**
+         * Método aplicado sobre el botón next.
+         * Consistirá en la rotación de 360 grados del imageView tras pulsar el botón next
+         * Además al pulsar sobre este, el reproductor pausará la canción actual y reanudará
+         * la siguiente
+         */
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mediaPlayer.stop();
+                mediaPlayer.release();
+
+                //position pasa a la siguiente canción de la lista dentro del array mySongs
+                position = ((position + 1)%mySongs.size());
+
+                //Uri convertirá el listado de canciones en un string para que podamos escucharlas
+                Uri uri = Uri.parse(mySongs.get(position).toString());
+
+                //Las siguientes líneas cambiarán el contexto del reproductor mostrando
+                //de manera visual el nombre de la siguiente pista de audio
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+                songName = mySongs.get(position).getName();
+                txtSongName.setText(songName);
+                mediaPlayer.start();
+
+                startAnimation(imageView, 360f);
+            }
+        });
+
+        /**
+         * Método aplicado sobre el botón previous.
+         * Consistirá en la rotación de -360 grados del imageView tras pulsar el botón previous
+         * Además al pulsar sobre este, el reproductor pausará la canción actual y reanudará
+         * la anterior
+         */
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mediaPlayer.stop();
+                mediaPlayer.release();
+
+                //position pasa a la canción anterior de la lista dentro del array mySongs
+                position = ((position - 1) < 0)?(mySongs.size() -1):position-1;
+                Uri uri = Uri.parse(mySongs.get(position).toString());
+
+                //Las siguientes líneas cambiarán el contexto del reproductor mostrando
+                //de manera visual el nombre de la siguiente pista de audio
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+                songName = mySongs.get(position).getName();
+                txtSongName.setText(songName);
+                mediaPlayer.start();
+
+                startAnimation(imageView, -360f);
+            }
+        });
+
+        //El método setOnclickListener para este botón permitirá que al pulsar sobre este
+        //podamos avanzar de manera rápida la canción que estemos escuchando
+        btnFastForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 10000);
+                }
+            }
+        });
+
+        //El método setOnclickListener para este botón permitirá que al pulsar sobre este
+        //podamos retroceder de manera rápida la canción que estemos escuchando
+        btnFastBackward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 10000);
+                }
+            }
+        });
+
+    }
+
+    /**
+     * Esta función nos permitirá mover el imageview de manera tactil a nuestro antojo
+     * @param view
+     * @param degree
+     */
+    public void startAnimation(View view, Float degree){
+        //Declaración e inicialización de un objeto de la clase ObjectAnimator
+        //Determinará el grado de rotación que adoptará el imageView en grados usando float
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(imageView, "rotation", 0f, degree);
+
+        //Determinación de la duración de la animación de la nota musical (la imageView)
+        objectAnimator.setDuration(1000);
+
+        //Declaración de un objeto de la clase AnimatorSet. Este dará inicio a la animación
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(objectAnimator);
+        animatorSet.start();
+    }
+
+    /**
+     * Está función nos dará el tiempo de la canción según dónde movamos la seekBar.
+     * @param duration
+     * @return
+     */
+    public String createTime(int duration){
+        String time = "";
+        int min = duration/1000/60;
+        int sec = duration/1000%60;
+
+        //Formato que va a adquirir el tiempo cuando movamos la seekBar
+        time = time + min + ":";
+
+        if (sec < 10) {
+            time+="0";
+        }
+
+        //Esta línea hará que la duración de la seekBar coja el formato adecuado
+        //Para que así marque los segundos que dura la canción según sobre cuál pulsemos
+        time+=sec;
+        return time;
+    }
+}
